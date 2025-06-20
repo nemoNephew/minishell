93ccb4f02c53dd13b2023fb29933117618f10e41
chym8 <81048965+chym8@users.noreply.github.com>
2025-06-20 15:56:31 +0100

    Update a.c

--- a.c
+++ a.c
@@ -1,2 +1,23 @@
-hgfhsdgsdg
-sdgsdgsfgsf
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(int ac, char **av)
+{
+	char	*c;
+	if (ac == 1)
+	{
+		while ((c = readline("minishell> ")) != 0)
+		{
+			if (*c)
+				add_history(c);
+			printf("Your command is : '%s'\n", c);
+			free(c);
+		}
+	}
+	else
+		return (1);
+	return (0);
+}
